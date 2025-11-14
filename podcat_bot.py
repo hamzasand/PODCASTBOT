753077bd68cab1af19d63d8727de78ba1fcdca3f
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2025-11-14 20:48:50 +0500

    Update podcat_bot.py

--- podcat_bot.py
+++ podcat_bot.py
@@ -1,9 +1,7 @@
 # Dateset main covid based dateset from huggiongface 
 # You can change datase according your needs
 import torch
-
 from datasets import load_dataset
-
 from transformers import BartForConditionalGeneration, BartTokenizer, Trainer, TrainingArguments
 # Step 2: Load Dataset
 dataset = load_dataset("mystic-leung/medical_cord19")

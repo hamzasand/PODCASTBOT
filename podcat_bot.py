f6681337c23d6d01d6ef0eead3740506c378fa9f
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-04-23 07:12:59 +0500

    Add weight decay and training parameters

--- podcat_bot.py
+++ podcat_bot.py
@@ -52,6 +52,7 @@ training_args = TrainingArguments(
     per_device_train_batch_size=4,
     
     per_device_eval_batch_size=4,
+    
     weight_decay=0.01,
     save_total_limit=3,
     num_train_epochs=20,
